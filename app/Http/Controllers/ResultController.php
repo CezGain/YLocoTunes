70e17a7aa79073e86088dee496141fe14ace5550
lukas <lukas.cristofaro@ynov.com>
2023-10-05 11:01:40 +0200

    add artist grid

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -9,12 +9,19 @@ class ResultController extends Controller
     public function showGrid()
     {
         $data = [
-            ['id' => 1, 'name' => 'Artiste 1', 'description' => 'Musique 1'],
-            ['id' => 2, 'name' => 'Artiste 2', 'description' => 'Musique 2'],
-            ['id' => 3, 'name' => 'Artiste 3', 'description' => 'Musique 3'],
-            
+            ['id' => 1, 'name' => 'Artiste 1', 'musique' => 'Musique 1'],
+            ['id' => 2, 'name' => 'Artiste 2', 'musique' => 'Musique 2'],
+            ['id' => 3, 'name' => 'Artiste 3', 'musique' => 'Musique 3'],
+            ['id' => 1, 'name' => 'Artiste 1', 'musique' => 'Musique 1'],
+            ['id' => 2, 'name' => 'Artiste 2', 'musique' => 'Musique 2'],
+            ['id' => 3, 'name' => 'Artiste 3', 'musique' => 'Musique 3'],
+            ['id' => 1, 'name' => 'Artiste 1', 'musique' => 'Musique 1'],
+            ['id' => 2, 'name' => 'Artiste 2', 'musique' => 'Musique 2'],
+            ['id' => 3, 'name' => 'Artiste 3', 'musique' => 'Musique 3'],
+            ['id' => 1, 'name' => 'Artiste 1', 'musique' => 'Musique 1'],
+            ['id' => 2, 'name' => 'Artiste 2', 'musique' => 'Musique 2'],
+            ['id' => 3, 'name' => 'Artiste 3', 'musique' => 'Musique 3'],
         ];
-
         return view('results', compact('data'));
     }
 }
