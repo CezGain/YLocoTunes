7ebd44147f4761e3507a7200caa9ca46bd5a3ef2
LoanF <loanfrancois1@gmail.com>
2023-10-05 17:30:31 +0200

    adding home button in header bar + enlargement validate filter button

--- resources/views/components/header-bar.blade.php
+++ resources/views/components/header-bar.blade.php
@@ -1,8 +1,10 @@
 <div
     style="height: 10%; width: 100%; background-color: rgba(0, 0, 0, 0.25); display: flex; align-items:center; justify-content: flex-end; padding-right: 6%">
     @auth
+        <a href="{{ url('..') }}" style="color: white; font-size: 140%; margin-right: 20px">Accueil</a>  
         <a href="{{ url('/profile') }}" style="color: white; font-size: 140%;">Profil</a>
     @else
+        <a href="{{ url('..') }}" style="color: white; font-size: 140%; margin-right: 20px">Accueil</a> 
         <a href="{{ route('login') }}" style="color: white; font-size: 140%;">Connexion</a>
     @endauth
 </div>
