52b66438addb616a033f849e44f5fabf90ba0612
LoanF <loanfrancois1@gmail.com>
2023-10-06 10:38:17 +0200

    fix search button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,19 +7,71 @@
                     alt="logo" border="0" class="w-20 h-20">
             </div>
             <div class="p-4 m-10">
-                <input type="text" id="myInput" placeholder="Cliquez pour localiser" style="cursor: pointer;"
-                    class="text-center text-white bg-gray-800 min-w-[250px] p-2 border rounded-l-lg w-1/2" readonly
-                    required>
+                <input type="text" id="myInput" placeholder="Cliquez pour localiser"
+                    class="text-center text-white bg-gray-800 min-w-[250px] p-2 border rounded w-1/2"
+                    style="cursor: pointer;" readonly required/>
+                <br><br>
+                <div class="flex justify-between">
+                    <button id="button-3" onclick="goToEvents()" id="toEvent"><i class="fa-solid fa-magnifying-glass"></i> Événements</button>
+                    <button id="button-3" onclick="goToArtists()" id="toArtists"><i class="fa-solid fa-magnifying-glass"></i> Artistes</button>
+                </div>
             </div>
-            <div style="display: flex; justify-content:space-between;">
-                <div style="cursor: pointer; color: white; background-color: rgba(38, 38, 38, 1); border: solid 1px white; padding: 6px 12px; border-radius: 50px;"
-                    onclick="goToEvents()" id="toEvent">Evénements</div>
-                <div style="cursor: pointer; color: white; background-color: rgba(38, 38, 38, 1); border: solid 1px white; padding: 6px 12px; border-radius: 50px;"
-                    onclick="goToArtists()" id="toArtists">Artistes</div>
-            </div>
+
+            <style>
+                #button-3 {
+                  appearance: none;
+                  background-color: #2ea44f;
+                  border: 1px solid rgba(27, 31, 35, .15);
+                  border-radius: 6px;
+                  box-shadow: rgba(27, 31, 35, .1) 0 1px 0;
+                  box-sizing: border-box;
+                  color: #fff;
+                  cursor: pointer;
+                  display: inline-block;
+                  font-family: -apple-system,system-ui,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji";
+                  font-size: 14px;
+                  font-weight: 600;
+                  line-height: 20px;
+                  padding: 6px 16px;
+                  position: relative;
+                  text-align: center;
+                  text-decoration: none;
+                  user-select: none;
+                  -webkit-user-select: none;
+                  touch-action: manipulation;
+                  vertical-align: middle;
+                  white-space: nowrap;
+                }
+
+                .button-3:focus:not(:focus-visible):not(.focus-visible) {
+                  box-shadow: none;
+                  outline: none;
+                }
+
+                .button-3:hover {
+                  background-color: #2c974b;
+                }
+
+                .button-3:focus {
+                  box-shadow: rgba(46, 164, 79, .4) 0 0 0 3px;
+                  outline: none;
+                }
+
+                .button-3:disabled {
+                  background-color: #94d3a2;
+                  border-color: rgba(27, 31, 35, .1);
+                  color: rgba(255, 255, 255, .8);
+                  cursor: default;
+                }
+
+                .button-3:active {
+                  background-color: #298e46;
+                  box-shadow: rgba(20, 70, 32, .2) 0 1px 0 inset;
+                }
+            </style>
+
             <script>
                 function goToEvents() {
-                    console.log(isLocation());
                     if (!isLocation()) return;
                     window.location.href = "/events";
                     return false;
