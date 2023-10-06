49d96b054962ae1f50152670994d7817ab6cf8ca
LoanF <loanfrancois1@gmail.com>
2023-10-06 09:53:28 +0200

    Rename filters

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -20,7 +20,7 @@ class ViewController extends Controller
     {
 
         return view('filters', [
-            'musicStyle' => ["Rock", "Electro", "Pop", "Jazz", "Hip-Hop", "Japan", "Electropop", "Techno", "Ambient", "Classique", "Punk", "Metal", "Pop Rock", "Folk", "Country", "House", "Hard-Rock", "Soul", "RnB", "Reggae", "Blues", "Disco", "Indie", "Soundtrack", "Heavy Metal", "Experimental", "Pop/Rock", "Alternative", "Rap", "Score", "Hardcore", "Lo-Fi", "Pop Punk", "Country Rock", "Punk Rock", "Drum and Bass", "Jazz Fusion", "Horror Punk", "Christmas", "Goth Rock", "Acoustic", "Electro-Acoustique" ],
+            'musicStyle' => ["Rock", "Electro", "Pop", "Jazz", "Hip-Hop", "Japan", "Electropop", "Techno", "Ambient", "Classique", "Punk", "Metal", "Pop Rock", "Folk", "Country", "House", "Hard-Rock", "Soul", "RnB", "Reggae", "Blues", "Disco", "Indie", "Soundtrack", "Heavy Metal", "Experimental", "Pop/Rock", "Alternative", "Rap", "Score", "Hardcore", "Lo-Fi", "Pop Punk", "Country Rock", "Punk Rock", "Drum and Bass", "Jazz Fusion", "Horror Punk", "Christmas", "Goth Rock", "Acoustic", "Electro-Acoustic" ],
             "colors" => ["rgba(238, 130, 130, 1)", "rgba(130, 186, 238, 1)", "rgba(130, 238, 141, 1)", "rgba(210, 238, 130, 1)", "rgba(238, 130, 163, 1)", "rgba(130, 238, 225, 1)", "rgba(130, 135, 238, 1)", "rgba(238, 208, 130, 1)", "rgba(238, 189, 130, 1)"],
         ]);
     }
